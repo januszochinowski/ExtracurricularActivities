ea96d71c9f1acea52231c48277ef825153416dbd
laptop <user@poczta.pl>
2026-01-24 15:11:30 +0100

    Initial commit

---
+++ src/main/java/com/example/extracurricularactivities/ExtracurricularActivitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.extracurricularactivities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtracurricularActivitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtracurricularActivitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/extracurricularactivities/ExtracurricularActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extracurricularactivities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtracurricularActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
